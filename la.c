32adf6c51be2ca9ea5f1732e60d0f3c31baeb102
Sevske <tookieblade@gmail.com>
2013-10-12 02:02:20 -0400

    added the struct from main.c back in and moved all the tables back into the main function

--- la.c
+++ la.c
@@ -15,35 +15,20 @@
 #define NESTMAX 5           // Maximum depth of block nesting
 #define STRMAX 256          // Maximum length of strings
 
+// Structs
+typedef struct nameRecord {
+    int kind;                   // const = 1, var = 2, proc = 3
+    char name[10];              // Name (max length: 11 chars)
+    int val;                    // Number (ASCII value)
+    int level;                  // L level
+    int addr;                   // M address
+    struct nameRecord *next;    // Linked list allocator
+} NameRecordTable;
+
 // Globals
 
 // Position of the program currently being processed
 int currentpos;
-
-// Defining token types
-typedef enum {
-	nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6,
-	slashsym = 7, oddsym = 8,  eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12,
-	gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
-	semicolonsym = 18, periodsym = 19, becomessym = 20, beginsym = 21, endsym = 22,
-	ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27, constsym = 28,
-	intsym = 29, procsym = 30, writesym = 31, readsym = 32, elsesym = 33
-} token_type;
-
-// Defining reserved words
-char *word[] = { "const", "int", "procedure", "call", "begin", "end", "if",
-							"then", "else", "while", "do", "read", "write" };
-// Defining word symbols integer array
-// Example: wordsym[2] contains reference to "procsym" which will output 30.
-int wordsym[] = { constsym, intsym, procsym, callsym, beginsym, endsym, ifsym,
-							 thensym, elsesym, whilesym, dosym, readsym, writesym };
-// Defining reserved symbols
-char symbols[] = { '+', '-', '*', '/', '(', ')', '=', ',', '.', '<', '>', ';', ':' };
-
-// Defining symbol symbols integer array
-// Example: symbolsym[2] contains reference to "multsym" which will output 6.
-int symbolsym[] = { plussym, minussym, multsym, slashsym, lparentsym, rparentsym,
-							  eqlsym, commasym, periodsym, lessym, gtrsym, semicolonsym, becomessym };
 							  
 void comments(char* program);
 
@@ -53,6 +38,31 @@ int main() {
     // File pointers for input and output files
     FILE *input = fopen("InputFile.txt", "r");
     FILE *output = fopen("OutputFile.txt", "w");
+    
+	// Defining token types
+	typedef enum {
+		nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6,
+		slashsym = 7, oddsym = 8,  eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12,
+		gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
+		semicolonsym = 18, periodsym = 19, becomessym = 20, beginsym = 21, endsym = 22,
+		ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27, constsym = 28,
+		intsym = 29, procsym = 30, writesym = 31, readsym = 32, elsesym = 33
+	} token_type;
+
+	// Defining reserved words
+	char *word[] = { "const", "int", "procedure", "call", "begin", "end", "if",
+								"then", "else", "while", "do", "read", "write" };
+	// Defining word symbols integer array
+	// Example: wordsym[2] contains reference to "procsym" which will output 30.
+	int wordsym[] = { constsym, intsym, procsym, callsym, beginsym, endsym, ifsym,
+								 thensym, elsesym, whilesym, dosym, readsym, writesym };
+	// Defining reserved symbols
+	char symbols[] = { '+', '-', '*', '/', '(', ')', '=', ',', '.', '<', '>', ';', ':' };
+
+	// Defining symbol symbols integer array
+	// Example: symbolsym[2] contains reference to "multsym" which will output 6.
+	int symbolsym[] = { plussym, minussym, multsym, slashsym, lparentsym, rparentsym,
+								  eqlsym, commasym, periodsym, lessym, gtrsym, semicolonsym, becomessym };
 
 	char program[10000];
 	int c, length = 0;
